870848593e4d849b72ebac9636a61b237533c752
Sidharth <sidforjee@gmail.com>
2021-04-30 10:53:40 +0530

    added allow_reload environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 # Migrate
 alembic upgrade head
 # Run
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-80}
+if ${ALLOW_RELOAD:-"false"} == "true"
+then
+    echo "ALLOW_RELOAD"
+    uvicorn app.main:app --reload --host 0.0.0.0 --port ${PORT:-80}
+else
+  uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-80}
+fi
