e31e304a02027f63f2f1d7cad479466ba3564e21
Sidharth <sidforjee@gmail.com>
2021-05-01 22:36:51 +0530

    minor fix

--- app/database/database.py
+++ app/database/database.py
@@ -11,6 +11,7 @@ def set_up_database(env_variable="DATABASE_URL"):
 
     if database_url is None:
         print("No %s exist" % env_variable)
+        return None
 
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
