0b41bfbdb5cb3ad3d2494fc222bb1199f7466b9b
Sidharth <sidforjee@gmail.com>
2021-05-01 22:44:40 +0530

    gave up trying to run without db, made test and production db same for github workflow

--- app/database/config_db.py
+++ app/database/config_db.py
@@ -1,13 +1,9 @@
 """
 Config Normal Database
 """
-from os import environ
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .database import set_up_database
 
-if environ.get("DATABASE_URL"):
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=set_up_database())
-else:
-    print("WARNING: No Production DB Detected")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=set_up_database())
 
 Base = declarative_base()
